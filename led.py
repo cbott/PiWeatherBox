bb8a55c783e8d433c1231a86ac772e30d1132f56
cbott <cbott6@gmail.com>
2017-05-27 16:58:16 +0000

    thread test

--- led.py
+++ led.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as gpio
 from time import sleep
+from threading import Thread
 
 BLUE_PIN = 21
 
@@ -13,13 +14,10 @@ p = gpio.PWM(BLUE_PIN, 50) # pin, freq (Hz)
 p.start(0)
 
 try:
+    blue = LED()
+    blue.fade()
     while 1:
-        for dc in range(0, 101, 5):
-            p.ChangeDutyCycle(dc)
-            sleep(0.1)
-        for dc in range(100, -1, -5):
-            p.ChangeDutyCycle(dc)
-            sleep(0.1)
+        raw_input("Still doing things! ")
 
 except KeyboardInterrupt:
     pass
@@ -28,3 +26,20 @@ finally:
     print "Cleaning up"
     p.stop()
     gpio.cleanup()
+
+class LED():
+    def __init__(self):
+        pass
+
+    def fade(self):
+        _t = Thread(self._fade)
+        _t.start()
+
+    def _fade(self):
+        while 1:
+            for dc in range(0, 101, 5):
+                p.ChangeDutyCycle(dc)
+	        sleep(0.1)
+            for dc in range(100, -1, -5):
+                p.ChangeDutyCycle(dc)
+                sleep(0.1)
