fe3fe98297f4ee07311a03120275671f10ce3c4f
cbott <cbott6@gmail.com>
2019-05-06 18:10:53 -0400

    update to use DarkSky weather API

--- weather.py
+++ weather.py
@@ -1,12 +1,12 @@
-import json
+import requests
 import time
-import urllib
-from urllib.request import urlopen
 
 API_KEY = ""
 with open("apikey.txt", "r") as f:
     API_KEY = f.readline().strip()
 
+
+ONE_DAY = 60 * 60 * 24  # Seconds in a day
 
 def conditions():
     """ Return relevant weather conditions for yesterday, today, and tomorrow
@@ -15,29 +15,41 @@ def conditions():
     { yesterday_high, today_high, today_rain, today_text, tomorrow_high, tomorrow_rain }
     or None if an error occurs
     """
-    url_yesterday = 'http://api.wunderground.com/api/%s/geolookup/yesterday/q/MI/Ann_Arbor.json' % API_KEY
-    url_forecast = 'http://api.wunderground.com/api/%s/geolookup/forecast/q/MI/Ann_Arbor.json' % API_KEY
+    lat = 42.262151
+    lon = -83.706412
+    exc = "currently,minutely,hourly,alerts"
+
+    url_yesterday = "https://api.darksky.net/forecast/{apikey}/{latitude},{longitude},{time}?exclude={exclude}"
+    url_forecast = "https://api.darksky.net/forecast/{apikey}/{latitude},{longitude}?exclude={exclude}"
 
     try:
-        yesterday = json.loads(urlopen(url_yesterday).read().decode('utf8'))
-        forecast = json.loads(urlopen(url_forecast).read().decode('utf8'))
+        yesterday_time = int(time.time() - ONE_DAY)  # UNIX timestamp of some point during yesterday
+        yesterday_resp = requests.get(url_yesterday.format(apikey=API_KEY, latitude=lat, longitude=lon, time=yesterday_time, exclude=exc))
+        if yesterday_resp.status_code != 200:
+            print(time.strftime("Invalid response status <{}> getting info for yesterday on %B %d at %H:%M:%S --".format(yesterday_resp.status_code)))
+            return None
+        yesterday = yesterday_resp.json()
 
-        yesterday_high = float(yesterday['history']['dailysummary'][0]['maxtempi'])
+        forecast_resp = requests.get(url_forecast.format(apikey=API_KEY, latitude=lat, longitude=lon, exclude=exc))
+        if forecast_resp.status_code != 200:
+            print(time.strftime("Invalid response status <{}> getting forecast on %B %d at %H:%M:%S --".format(forecast_resp.status_code)))
+            return None
+        forecast = forecast_resp.json()
 
-        today_high = float(forecast['forecast']['simpleforecast']['forecastday'][0]['high']['fahrenheit'])
-        today_rain = float(forecast['forecast']['simpleforecast']['forecastday'][0]['qpf_allday']['in'])
-        today_text = forecast['forecast']['txt_forecast']['forecastday'][0]['fcttext']
+        yesterday_high = float(yesterday["daily"]["data"][0]["temperatureHigh"])
 
-        tomorrow_high = float(forecast['forecast']['simpleforecast']['forecastday'][1]['high']['fahrenheit'])
-        tomorrow_rain = float(forecast['forecast']['simpleforecast']['forecastday'][1]['qpf_allday']['in'])
+        today_high = float(forecast["daily"]["data"][0]["temperatureHigh"])
+        today_rain = float(forecast["daily"]["data"][0].get("precipIntensityMax", -1))  # inches of liquid water per hour
+        today_text = forecast["daily"]["data"][0].get("summary", "")
+
+        tomorrow_high = float(forecast["daily"]["data"][1]["temperatureHigh"])
+        tomorrow_rain = float(forecast["daily"]["data"][1].get("precipIntensityMax", -1))
 
         return {'yesterday': {'high': yesterday_high},
                 'today': {'high': today_high, 'rain': today_rain, 'conditions': today_text},
                 'tomorrow': {'high': tomorrow_high, 'rain': tomorrow_rain}}
-    except urllib.error.URLError as e:
-        print(time.strftime("Error contacting Wunderground API on %B %d at %H:%M:%S --"), e)
     except ValueError as e:  # json.decoder.JSONDecodeError in python 3.5+
-        print(time.strftime("Error parsing Wunderground response on %B %d at %H:%M:%S --"), e)
+        print(time.strftime("Error parsing API response on %B %d at %H:%M:%S --"), e)
     except Exception as e:
         print(time.strftime("An unexpected error occurred while receiving weather data on %B %d at %H:%M:%S --"), e)
     return None
